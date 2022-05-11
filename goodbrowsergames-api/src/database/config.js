70b8d96160a85d433dc8728fd4ba05ad1eb83768
NoExceptions <gguerra@mail.ru>
2022-05-11 15:38:38 -0300

    main

--- goodbrowsergames-api/src/database/config.js
+++ goodbrowsergames-api/src/database/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    uri: "mongodb://localhost:27017/gbg"
+    uri: "mongodb+srv://api:jsokwiBQAL8Cev3Y@cluster0.lalre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   };
